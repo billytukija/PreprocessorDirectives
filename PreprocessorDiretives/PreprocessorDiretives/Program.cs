d163eadcdee5c69fd2814fc77a14cb2f95de92bd
Billy Tukija Richard <billytukija@gmail.com>
2019-07-17 16:17:04 -0300

    IF and PRAGMA

--- PreprocessorDiretives/PreprocessorDiretives/Program.cs
+++ PreprocessorDiretives/PreprocessorDiretives/Program.cs
@@ -1,16 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿#define MYTEST
+using System;
 
 namespace PreprocessorDiretives
 {
-    class Program
+#pragma warning disable 414, CS3021
+    [CLSCompliant(false)]
+    public class Program
     {
         static void Main(string[] args)
         {
-
+            #region IF
+#if (DEBUG && !MYTEST)
+                Console.WriteLine("DEBUG is defined");
+#elif (!DEBUG && MYTEST)
+        Console.WriteLine("MYTEST is defined");
+#elif (DEBUG && MYTEST)
+            Console.WriteLine("DEBUG and MYTEST are defined");
+#else
+        Console.WriteLine("DEBUG and MYTEST are not defined");
+#endif
         }
     }
+    #endregion
+
+    #region PRAGMA
+#pragma warning restore CS3021
+    [CLSCompliant(false)]  // CS3021  
+    public class D
+    {
+        int i = 1;
+        public static void F()
+        {
+            // Rules
+        }
+    }
+    #endregion
 }
